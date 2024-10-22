910454eaff43fbea85e7d7244164234b1719224d
77Dhruvil <pateldhruvil2119976@gmail.com>
2024-10-22 22:48:07 +0530

    Testcase of fovero login

---
+++ code/test_foverologin.py
@@ -0,0 +1,15 @@
+import pytest
+import allure
+
+from playwright.sync_api import Page ,expect, sync_playwright
+
+def test_foverologin():
+    #1. Browser and page
+    browser = sync_playwright().start().chromium.launch(headless=False)
+    page = browser.new_page()
+
+    #2. Code INteraction with the html web page
+    page.goto("https://fovero.app/signin")
+
+    #3. Validation
+    expect(page).to_have_title("Fovero")
